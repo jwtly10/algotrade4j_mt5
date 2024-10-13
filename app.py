87920d06f43a0c969b9b7f095949fd52239f8ac0
jwtly10 <39057715+jwtly10@users.noreply.github.com>
2024-10-13 17:53:18 +0100

    checkin - wip open trades mt5 endpoint

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ from typing import Tuple, Optional, Union
 
 import validation
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 log = logging.getLogger(__name__)
 
 app = Flask(__name__)
@@ -15,7 +17,10 @@ app = Flask(__name__)
 instances = {}
 closed_trades_cache = {}
 
-def init_mt5_instance(accountId: int, password: str, server: str, path: str) -> Tuple[bool, Optional[Tuple[int, str]]]:
+
+def init_mt5_instance(
+    accountId: int, password: str, server: str, path: str
+) -> Tuple[bool, Optional[Tuple[int, str]]]:
     """
     Initializes an MT5 instance with given account credentials.
 
@@ -32,7 +37,8 @@ def init_mt5_instance(accountId: int, password: str, server: str, path: str) -> 
     closed_trades_cache[accountId] = {}
     return True, None
 
-@app.route('/initialize', methods=['POST'])
+
+@app.route("/initialize", methods=["POST"])
 def initialize():
     """
     Initializes a MetaTrader 5 (MT5) account using provided account credentials.
@@ -51,7 +57,7 @@ def initialize():
           "status": "initialized",
           "message": "Successfully initialized account with id: <accountId>"
       }
-    
+
     - 400 Bad Request: If the input parameters are invalid, returns a JSON response indicating the validation errors.
       Example:
       {
@@ -68,12 +74,14 @@ def initialize():
       }
     """
     data = request.json
-    accountId = data['accountId']
-    password = data['password']
-    server = data['server']
-    path = data['path']
+    accountId = data["accountId"]
+    password = data["password"]
+    server = data["server"]
+    path = data["path"]
 
-    validation_error = validation.validate_initialise_params(accountId, password, server, path)
+    validation_error = validation.validate_initialise_params(
+        accountId, password, server, path
+    )
     if validation_error:
         return jsonify(validation_error), 400
 
@@ -83,13 +91,31 @@ def initialize():
         success, error = init_mt5_instance(accountId, password, server, path)
         if success:
             log.info(f"Successfully initialized account %s", accountId)
-            return jsonify({"status": "initialized", "message": f"Succesfully initialized account with id: {accountId}"}), 200
+            return jsonify(
+                {
+                    "status": "initialized",
+                    "message": f"Succesfully initialized account with id: {accountId}",
+                }
+            ), 200
         else:
-            err_str = log_error(error, f'/initialize [POST] with accountID: {accountId}')
-            return jsonify({"status": "failed", "message": f"Failed to initialize MetaTrader instance: {err_str}"}), 500
+            err_str = log_error(
+                error, f"/initialize [POST] with accountID: {accountId}"
+            )
+            return jsonify(
+                {
+                    "status": "failed",
+                    "message": f"Failed to initialize MetaTrader instance: {err_str}",
+                }
+            ), 500
     except Exception as e:
         log.error("Exception on /initialize [POST]", exc_info=True)
-        return jsonify({"status": "failed", "message": "An unexpected error occurred during initialization."}), 500
+        return jsonify(
+            {
+                "status": "failed",
+                "message": "An unexpected error occurred during initialization.",
+            }
+        ), 500
+
 
 def get_mt5_instance(accountId: int) -> Union[bool, Tuple[Response, int]]:
     """
@@ -101,16 +127,22 @@ def get_mt5_instance(accountId: int) -> Union[bool, Tuple[Response, int]]:
     """
     if accountId not in instances:
         log.error(f"Account id {accountId} not found in instances")
-        return jsonify({"status": "failed", "message": f"MT5 instance not initialized for account {accountId}"}), 404
+        return jsonify(
+            {
+                "status": "failed",
+                "message": f"MT5 instance not initialized for account {accountId}",
+            }
+        ), 404
     return True
 
-@app.route('/get-account/<int:accountId>', methods=["GET"])
+
+@app.route("/get-account/<int:accountId>", methods=["GET"])
 def get_account(accountId: int) -> Tuple[Response, int]:
     """
     Fetches account information for a specific MetaTrader 5 (MT5) account.
 
     :param accountId: The account ID (int) for which the account information is being fetched.
-    
+
     :return: A JSON response containing the account information if successful, or an error message with the corresponding HTTP status code.
 
     Response:
@@ -149,7 +181,7 @@ def get_account(accountId: int) -> Tuple[Response, int]:
     """
     log.info(f"Getting account info for account {accountId}")
     instance_check = get_mt5_instance(accountId)
-    
+
     # If the instance_check is not True, it means an error response is returned
     if instance_check is not True:
         return instance_check
@@ -160,19 +192,26 @@ def get_account(accountId: int) -> Tuple[Response, int]:
     if account:
         return jsonify(account._asdict()), 200
     else:
-        err_str = log_error(error, "/get-account/<int:accountId> [GET] with accountId: {accountId}")
-        return jsonify({"status": "failed", "message": f"Failed to fetch account information: {err_str}"}), 500
-    
+        err_str = log_error(
+            error, "/get-account/<int:accountId> [GET] with accountId: {accountId}"
+        )
+        return jsonify(
+            {
+                "status": "failed",
+                "message": f"Failed to fetch account information: {err_str}",
+            }
+        ), 500
 
-@app.route('/get-trades/<int:accountId>', methods=["GET"])
+
+@app.route("/get-trades/<int:accountId>", methods=["GET"])
 def get_trades(accountId):
     """
     Fetches open trades for a specific MetaTrader 5 (MT5) account.
 
     :param accountId: The account ID (int) for which trades are being requested.
-    
+
     :return: A JSON response with the following structure:
-    
+
     - 200 OK: If trades are successfully fetched.
       Example:
       {
@@ -195,61 +234,160 @@ def get_trades(accountId):
         trades_list = [trade._asdict() for trade in trades]
         return jsonify({"status": "success", "trades": trades_list}), 200
     elif trades is None:
-        err_str = log_error(error, f"/get-trades/<int:accountId> [GET] with accountId: {accountId}")
-        return jsonify({"status": "failed", "message": f"Failed to fetch trades: {err_str}"}), 500
+        err_str = log_error(
+            error, f"/get-trades/<int:accountId> [GET] with accountId: {accountId}"
+        )
+        return jsonify(
+            {"status": "failed", "message": f"Failed to fetch trades: {err_str}"}
+        ), 500
     else:
         return jsonify({"status": "success", "trades": {}}), 200
 
 
-# TODO
-@app.route('/open-trade/<int:accountId>', methods=["POST"])
+@app.route("/open-trade/<int:accountId>", methods=["POST"])
 def open_trade(accountId):
+    """
+    Note 13th Oct 2024:
+    Our current implementation does not support using data directly form the MT5 Instance. We use external data providers
+    that closesly resemble the data from the broker, however data may not match up perfectly. Therefore we need to change how
+    we actually trigger new trades when connecting to a MT5 terminal
+
+
+    The current implementation provides an interface to open a trade at the current price, give a specific stop
+    loss size, and delegates logic for generating the trade values to the python adapter.
+    data:
+    {
+    "instrument":"inst",
+    "quantity": 0.00,
+    "entryPrice":
+        {
+            value: 10000
+        },
+    "stopLoss":
+        {
+            value: 9000
+        },
+    "takeProfit":
+        {
+            value: 12000
+        },
+    "riskPercentage": 0.3,
+    "riskRatio": 2,
+    "balanceToRisk": 10000,
+    "isLong": 0.3,
+    "openTime": "something"
+    }
+
+    How it works is, it will take the price given by the caller, and if within a given tolerance (TODO)
+    will execute the trade with the same setup at market price
+
+    Take the above data example
+
+    The platform has already done its own logic based on the data provided to generate its own trade,
+    and now we need to match it up correctly to the real data we have from the broker:
+
+    1. Figure out the stop loss pips - abs(entryPrice-stoplossprice)
+    2. Create a new stop loss based on current market price, using isLong as bid/ask flag
+    3. Using the riskRatio create a new take profit based on the stop loss pips
+    4. Generate quantity based on balance to risk & riskPercentage, and the given stop loss
+    5. Execute trade
+
+
+
+    """
     instance_check = get_mt5_instance(accountId)
     if instance_check != True:
         return instance_check
 
     data = request.json
-    symbol = data['symbol']
-    volume = data['volume']
-    order_type = data['order_type']  # mt5.ORDER_TYPE_BUY or mt5.ORDER_TYPE_SELL
-    price = mt5.symbol_info_tick(symbol).ask if order_type == mt5.ORDER_TYPE_BUY else mt5.symbol_info_tick(symbol).bid
-    stop_loss = data.get('stop_loss', None)
-    take_profit = data.get('take_profit', None)
+    symbol = data["instrument"]
+    risk_percentage = data["riskPercentage"]
+    balance_to_risk = data["balanceToRisk"]
+    stop_loss_input = data["stopLoss"]["value"]
+    entry_price = data["entryPrice"]["value"]
+    take_profit_input = data["takeProfit"]["value"]
+    risk_ratio = data["riskRatio"]
+    is_long = data["isLong"] == 1
 
+    symbol_info = mt5.symbol_info_tick(symbol)
+    if not symbol_info:
+        return jsonify({"status": "failed", "message": "Symbol not found"}), 404
+
+    current_price = symbol_info.ask if is_long else symbol_info.bid
+
+    stop_loss_pips = abs(entry_price - stop_loss_input)
+
+    if is_long:
+        new_stop_loss = current_price - stop_loss_pips
+    else:
+        new_stop_loss = current_price + stop_loss_pips
+
+    new_take_profit_pips = stop_loss_pips * risk_ratio
+    if is_long:
+        new_take_profit = current_price + new_take_profit_pips
+    else:
+        new_take_profit = current_price - new_take_profit_pips
+
+    # TODO: Pip value conversion from Algotrade4j to Adapter
+    risk_amount = balance_to_risk * risk_percentage
+    volume = risk_amount / (stop_loss_pips * 10)
+
+    order_type = mt5.ORDER_TYPE_BUY if is_long else mt5.ORDER_TYPE_SELL
     request = {
         "action": mt5.TRADE_ACTION_DEAL,
         "symbol": symbol,
         "volume": volume,
         "type": order_type,
-        "price": price,
-        "sl": stop_loss,
-        "tp": take_profit,
+        "price": current_price,
+        "sl": new_stop_loss,
+        "tp": new_take_profit,
     }
-    
+
     result = mt5.order_send(request)
-    
+
     if result and result.retcode == mt5.TRADE_RETCODE_DONE:
         return jsonify(result._asdict()), 200
-    return jsonify({"status": "failed", "message": "Failed to open trade"}), 500
+    else:
+        error = mt5.last_error()
+        err_str = log_error(
+            error,
+            f"/open-trade/<int:accountId> [POST] with accountId: {accountId} and data: {data}",
+        )
+        return jsonify(
+            {
+                "status": "failed",
+                "message": f"Failed to open trade: [{result.retcode if result else "Unknown Error"}] {err_str}",
+                "error_code": result.retcode if result else "Unknown error",
+            }
+        ), 500
+
 
 # TODO
-@app.route('/close-trade/<int:accountId>', methods=["POST"])
+@app.route("/close-trade/<int:accountId>", methods=["POST"])
 def close_trade(accountId):
     instance_check = get_mt5_instance(accountId)
     if instance_check != True:
         return instance_check
 
     data = request.json
-    ticket = data['ticket']
-    symbol = data['symbol']
+    ticket = data["ticket"]
+    symbol = data["symbol"]
     position = mt5.positions_get(ticket=ticket)
 
     if not position:
         return jsonify({"status": "failed", "message": "Trade not found"}), 404
 
-    close_type = mt5.ORDER_TYPE_SELL if position[0].type == mt5.ORDER_TYPE_BUY else mt5.ORDER_TYPE_BUY
-    close_price = mt5.symbol_info_tick(symbol).bid if close_type == mt5.ORDER_TYPE_SELL else mt5.symbol_info_tick(symbol).ask
-    
+    close_type = (
+        mt5.ORDER_TYPE_SELL
+        if position[0].type == mt5.ORDER_TYPE_BUY
+        else mt5.ORDER_TYPE_BUY
+    )
+    close_price = (
+        mt5.symbol_info_tick(symbol).bid
+        if close_type == mt5.ORDER_TYPE_SELL
+        else mt5.symbol_info_tick(symbol).ask
+    )
+
     request = {
         "action": mt5.TRADE_ACTION_DEAL,
         "symbol": symbol,
@@ -257,15 +395,16 @@ def close_trade(accountId):
         "type": close_type,
         "price": close_price,
     }
-    
+
     result = mt5.order_send(request)
 
     if result and result.retcode == mt5.TRADE_RETCODE_DONE:
         return jsonify(result._asdict()), 200
     return jsonify({"status": "failed", "message": "Failed to close trade"}), 500
 
+
 # TODO
-@app.route('/transactions-stream/<int:accountId>', methods=["GET"])
+@app.route("/transactions-stream/<int:accountId>", methods=["GET"])
 def closed_trades_stream(accountId):
     instance_check = get_mt5_instance(accountId)
     if instance_check != True:
@@ -275,7 +414,9 @@ def closed_trades_stream(accountId):
         while True:
             time.sleep(1)
             now = time.time()
-            closed_trades = mt5.history_deals_get(datetime.now() - timedelta(seconds=1), datetime.now())
+            closed_trades = mt5.history_deals_get(
+                datetime.now() - timedelta(seconds=1), datetime.now()
+            )
 
             if closed_trades:
                 for trade in closed_trades:
@@ -283,7 +424,8 @@ def closed_trades_stream(accountId):
                         closed_trades_cache[accountId][trade.ticket] = now
                         yield f"data: {trade._asdict()}\n\n"
 
-    return Response(generate_closed_trades(accountId), content_type='text/event-stream')
+    return Response(generate_closed_trades(accountId), content_type="text/event-stream")
+
 
 def log_error(err: Tuple[int, str], action: str) -> str:
     """
@@ -291,12 +433,12 @@ def log_error(err: Tuple[int, str], action: str) -> str:
 
     :param err: A tuple containing the error code (int) and the error message (str).
     :param action: The action that failed (str)
-    
+
     :return: A formatted string describing the error in the form:
              "Error code: <error_code>, Reason: <error_message>"
 
     Logs:
-    - Error: Logs an error with the format "Error during <action>: Error code: <error_code>, Reason: <error_message>" to indicate the failure 
+    - Error: Logs an error with the format "Error during <action>: Error code: <error_code>, Reason: <error_message>" to indicate the failure
       during operations.
     """
     err_str = f"Error code: {err[0]}, Reason: {err[1]}"
@@ -304,5 +446,5 @@ def log_error(err: Tuple[int, str], action: str) -> str:
     return err_str
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
