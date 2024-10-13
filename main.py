d36a40f76318274353a62f8bacb06a8a84d99886
jwtly10 <39057715+jwtly10@users.noreply.github.com>
2024-10-13 18:58:44 +0100

    add basic api key auth

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, Header
 from routes.account import router as account_router
+from dotenv import load_dotenv
+import os
+
 from routes.trades import router as trades_router
 from routes.transactions import router as transactions_router
 
+
+load_dotenv()
+
 app = FastAPI()
 
-app.include_router(account_router, prefix="/account")
-app.include_router(trades_router, prefix="/trades")
-app.include_router(transactions_router, prefix="/trades")
+API_KEY = os.getenv("AUTH_API_KEY")
+API_KEY_HEADER = "X-API-KEY"
 
 
-@app.get("/")
-async def root():
-    return {"message": "Welcome to the Algotrade4j MT5 adapter using FastAPI"}
+async def api_key_dependency(x_api_key: str = Header(...)):
+    if x_api_key != API_KEY:
+        raise HTTPException(status_code=403, detail="Could not validate API KEY")
+
+
+app.include_router(
+    account_router, prefix="/api/v1", dependencies=[Depends(api_key_dependency)]
+)
+app.include_router(
+    trades_router, prefix="/api/v1", dependencies=[Depends(api_key_dependency)]
+)
+app.include_router(
+    transactions_router, prefix="/api/v1", dependencies=[Depends(api_key_dependency)]
+)
+
+
+@app.get("/api/v1/health")
+async def health():
+    return {"status": "healthy"}
 
 
 if __name__ == "__main__":
